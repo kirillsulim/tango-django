4f7c5267cea68a333d101a9675627cf199ceafbf
kirillsulim <kirillsulim@gmail.com>
2015-01-22 16:51:05 +0500

    Chapter 4: Excercises

--- rango/views.py
+++ rango/views.py
@@ -2,4 +2,7 @@ from django.shortcuts import render
 from django.http import HttpResponse
 
 def index(request):
-    return HttpResponse("Hi! I'm ducking work!")
+    return HttpResponse('Hi! I''m ducking work! <br/><a href = "./about/">About</a>')
+
+def about(request):
+    return HttpResponse('Hi! It''s ducking about! <br/><a href = "../">Go home</a>')
