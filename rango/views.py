b4021cdf27250dcbf5b65a0ce63895730c5d1207
kirillsulim <kirillsulim@gmail.com>
2015-02-09 21:51:12 +0500

    Chapter 8

--- rango/views.py
+++ rango/views.py
@@ -3,8 +3,11 @@ from django.http import HttpResponse
 from django.shortcuts import render
 
 from rango.models import Page
-
 from rango.models import Category
+
+from rango.forms import CategoryForm
+from rango.forms import PageForm
+
 
 def index(request):
     category_list_likes = Category.objects.order_by("-likes")[:5]
@@ -14,6 +17,7 @@ def index(request):
         "categories_views": category_list_views,
     }
     return render(request, 'rango/index.html', context_dict)
+
 
 def category(request, category_name_slug):
     context_dict = {}
@@ -31,5 +35,46 @@ def category(request, category_name_slug):
 
     return render(request, 'rango/category.html', context_dict)
 
+
 def about(request):
     return  render(request, 'rango/about.html')
+
+
+def add_category(request):
+    if request.method == "POST":
+        form = CategoryForm(request.POST)
+        if form.is_valid():
+            form.save(commit=True)
+            return index(request)
+        else:
+            print(form.errors)
+    else:
+        form = CategoryForm()
+
+    return render(request, "rango/add_category.html", {"form": form})
+
+
+def add_page(request, category_name_slug):
+    try:
+        cat = Category.objects.get(slug=category_name_slug)
+    except Category.DoesNotExist:
+        cat = None
+
+    if request.method == "POST":
+        form = PageForm(request.POST)
+        if form.is_valid():
+            if cat:
+                page = form.save(commit=False)
+                page.category = cat
+                page.save()
+
+                return category(request, category_name_slug)
+            else:
+                return index(request)
+        else:
+            print(form.errors)
+    else:
+        form = PageForm()
+
+    return render(request, "rango/add_page.html", {"form": form, "category": cat, })
+
